719fc96db53326305889db6af90dc9ec5cfe677c
Greg Cole <gcole@palantir.com>
2021-06-23 13:27:05 -0700

    Rewrite - 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,15 @@
 import 'babel-polyfill';
-import * as React from 'react';
+import React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
-import { Provider } from 'mobx-react';
-import { Languages } from './language/languages';
-import { getCookie, deleteCookie } from './utils/cookies';
-import { TOKEN_HEADER, apiPath } from './api/config';
-import { SessionData, SessionStore } from './state/SessionStore';
-import { IUser } from './api/api';
 import { App } from './components/App';
 import { registerGlobaListener } from './dropdownListener';
-import { create } from 'apisauce';
-import { ApisauceWrapper } from './api/network';
 import { browserHistory } from './history';
-import { StoreContext } from './state/StoreContext';
-require('./index.scss');
+import './index.scss';
+import { Languages } from './language/languages';
+import { SessionData } from './state/SessionStore';
+import { getCookie } from './utils/cookies';
+import { Provider } from 'react-redux';
 
 export const SESSION_COOKIE = 'TSUMEGO_KAI_TOKEN';
 export const LANGUAGE_COOKIE = 'TSUMEGO_KAI_LANGUAGE';
@@ -25,38 +20,29 @@ async function setup() {
     const language = getCookie(LANGUAGE_COOKIE, DEFAULT_LANGUAGE) || DEFAULT_LANGUAGE;
     let sessionData: SessionData | undefined;
 
-    const api = new ApisauceWrapper(create({
-        baseURL: apiPath(''),
-    }));
-
-    if (existingToken) {
-        api.setHeader(TOKEN_HEADER, existingToken);
-        try {
-            const response = await api.get('/user');
-            if (response.ok && response.data) {
-                const user = response.data as IUser;
-                sessionData = {
-                    token: existingToken,
-                    userId: user.id,
-                    login: user.login,
-                    firstName: user.firstName,
-                    lastName: user.lastName,
-                    rank: user.rank,
-                    roles: user.roles,
-                };
-            }
-        } catch (error) {
-            deleteCookie(SESSION_COOKIE);
-        }
-    }
-
-    const context: StoreContext = {
-        api,
-    };
-    const sessionStore = new SessionStore(context, language, sessionData);
+    // if (existingToken) {
+    //     api.setHeader(TOKEN_HEADER, existingToken);
+    //     try {
+    //         const response = await api.get('/user');
+    //         if (response.ok && response.data) {
+    //             const user = response.data as IUser;
+    //             sessionData = {
+    //                 token: existingToken,
+    //                 userId: user.id,
+    //                 login: user.login,
+    //                 firstName: user.firstName,
+    //                 lastName: user.lastName,
+    //                 rank: user.rank,
+    //                 roles: user.roles,
+    //             };
+    //         }
+    //     } catch (error) {
+    //         deleteCookie(SESSION_COOKIE);
+    //     }
+    // }
 
     ReactDOM.render((
-        <Provider sessionStore={sessionStore}>
+        <Provider store={null as any}>
             <Router history={browserHistory}>
                 <App/>
             </Router>
